a64291df1c8d3b78eeba6ab0661a05a81399960a
GabrielaParedes27 <78928248+GabrielaParedes27@users.noreply.github.com>
2021-03-15 15:58:44 -0600

    weather conditions

--- js.js
+++ js.js
@@ -109,7 +109,8 @@ function currentLocation(position) {
   let currentLong = position.coords.longitude;
   let apiKey = "816a63a33af440332c05784e3d9896ea";
   let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${currentLat}&lon=${currentLong}&appid=${apiKey}&units=metric`;
-  axios.get(${apiUrl}).then(currentCityData);
+
+  axios.get(`${apiUrl}`).then(showWeatherConditions);
 }
 
 function updateLocation(event) {
